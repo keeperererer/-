86a1b6d0a381b52d47c0937c73fdc5fb39e97c60
keeperererer <2249269086@qq.com>
2019-06-24 19:12:49 +0800

    save

---
+++ 微信小程序/登录/miniprogram/pages/login/login.js
@@ -0,0 +1,45 @@
+
+var app = getApp();
+
+Page({
+  data: {
+    username: null,
+    password: null,
+  },
+  onLoad: function (options) {
+
+  },
+  onReady: function () {
+
+  },
+  onShow: function () {
+    // 页面显示
+  },
+  onHide: function () {
+    // 页面隐藏
+  },
+  onUnload: function () {
+    // 页面关闭
+  },
+
+  loginBtnClick: function () {
+
+    // 用户名和密码验证的过程
+    console.log("click")
+    app.appData.userinfo = { username: this.data.username, password: this.data.password }
+    wx.switchTab({ url: "../user/user" })
+    console.log("clicked")
+  },
+
+  usernameInput: function (event) {
+
+
+    this.setData({ username: event.detail.value })
+  },
+
+  passwordInput: function (event) {
+    this.setData({ password: event.detail.value })
+  }
+
+
+})
---
+++ 微信小程序/首页/miniprogram/pages/index/index.js
@@ -0,0 +1,15 @@
+Page({
+  data: {
+    indicatorDots:true,
+    autoplay:true,
+    interval:5000,
+    duration:1000,
+    imgUrls:[
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ],
+    contentItems: ['', '', '', ''],
+    listItems: ['', '', '', '', '', '', '']
+  }
+})
